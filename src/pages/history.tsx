51983b9e8ad96c7195107acc9d58ee0c0d107e3f
hiroki0816 <hiroki.oosugi@gmail.com>
2021-05-03 22:06:43 +0900

    create history page

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -2,14 +2,30 @@ import * as React from "react";
 import styled from "styled-components";
 import { Link, useHistory } from "react-router-dom";
 import { Header } from "../components/header";
+import { getMemos, MemoRecord } from "../indexeddb/memos";
+
+const { useState, useEffect } = React;
 
 export const History: React.FC = () => {
+  const [memos, setMemos] = useState<MemoRecord[]>([]);
+
+  useEffect(() => {
+    getMemos().then(setMemos);
+  }, []);
+
   return (
     <>
       <Header title="History">
         <LinkText to="/editor">Back to Editor</LinkText>
       </Header>
-      <Wrapper>TODO: 履歴表示</Wrapper>
+      <Wrapper>
+        {memos.map((memo) => (
+          <Memo key={memo.datetime}>
+            <MemoTitle>{memo.title}</MemoTitle>
+            <MemoText>{memo.text}</MemoText>
+          </Memo>
+        ))}
+      </Wrapper>
     </>
   );
 };
@@ -23,9 +39,8 @@ const Wrapper = styled.div`
   background: #121212;
   padding: 10px 20px;
   color: #ffffff;
+  overflow: scroll;
 `;
-
-
 
 const LinkText = styled(Link)`
   background-color: transparent;
@@ -55,4 +70,37 @@ const LinkText = styled(Link)`
   &:active {
     text-decoration: none;
   }
+`;
+
+const Memo = styled.button`
+  display: block;
+  background: #232323;
+  border: none;
+  outline: none;
+  border-radius: 4px;
+  color: #ffffff;
+  width: 100%;
+  padding: 1.15rem;
+  margin: 1rem 0;
+  text-align: left;
+  cursor: pointer;
+  transition: 0.2s;
+
+  &:hover {
+    background: #2e2e2e;
+  }
+`;
+
+const MemoTitle = styled.div`
+  font-size: 1.15rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+`;
+
+const MemoText = styled.div`
+  font-size: 0.85rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  color:#A0A0A0;
+  white-space: nowrap;
 `;
